fafa278c92fca8c7c2c8e381bb58e1a7a110b14d
guillenjs <jorgeguilln@gmail.com>
2020-07-12 17:59:03 -0400

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+
+class GetRequester
+    attr_accessor :URL
+
+    def initialize(string)
+       @URL = string
+        
+    end
+
+    def get_response_body
+        uri = URI.parse(@URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        JSON.parse(get_response_body)
+    end
+end
